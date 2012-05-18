d731138c99f6902761c6cee8ca45ae69f5b1b29e
ferronrsmith@gmail.com <ferronrsmith@gmail.com@228877c5-d6cc-df6e-e1fa-38e68723f89d>
2012-05-18 06:15:18 +0000

    change master page

--- SalonComplex/Master/Site.Master.designer.cs
+++ SalonComplex/Master/Site.Master.designer.cs
@@ -49,6 +49,15 @@ namespace SalonComplex.Master {
         protected global::System.Web.UI.HtmlControls.HtmlAnchor Username;
         
         /// <summary>
+        /// ChangePassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor ChangePassword;
+        
+        /// <summary>
         /// HeadLoginView control.
         /// </summary>
         /// <remarks>
